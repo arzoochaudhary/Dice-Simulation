1a49b5b1284d32645cd57a2dc92a120e2c42b7bf
arzoochaudhary <arzoochaudhary9545@gmail.com>
2025-06-22 19:13:54 +0530

    refactored the code

--- DiceSimulation/diceSimulator(PSClab).py
+++ DiceSimulation/diceSimulator(PSClab).py
@@ -1,71 +1,72 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jun 21 11:12:16 2021
-
 @author: Arzoo
+Modularized without changing original layout
 """
+
 import random
+import os
+import tkinter as tk
 from PIL import Image, ImageTk
-import tkinter
 
-root=tkinter.Tk()
-root.geometry('400x400')
-root.title('Dice Simulation')
+# --- Constants ---
+DICE = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png']
+SCRIPT_DIR = os.path.dirname(__file__)
 
-line=tkinter.Label(root,text="")
-line.pack()
+# --- Utility Functions ---
+def pick_image(biased=False):
+    """Return file path for dice image, biased adds extra '6's."""
+    choices = DICE + (['6.png'] * 2 if biased else [])
+    return os.path.join(SCRIPT_DIR, random.choice(choices))
 
-heading1=tkinter.Label(root,text='Dice Simulator',fg='white',bg='black',font='100')
-heading1.pack()
-heading1.config(font=("",44))
-heading1.config(width=200)
-line=tkinter.Label(root,text="")
-line.pack()
+def prepare_open_path(fp):
+    """Normalize Windows backslashes to forward slashes."""
+    return fp.replace('\\', '/')
 
+def load_photo(filepath):
+    return ImageTk.PhotoImage(Image.open(filepath))
 
+# --- GUI Setup ---
+def setup_root():
+    root = tk.Tk()
+    root.geometry('400x400')
+    root.title('Dice Simulation')
+    return root
 
-"""
-alldice=ImageTk.PhotoImage(Image.open("C:/Users/Arzoo/Desktop/DiceSimulation/allDice.png"))
-label=tkinter.Label(root,image=alldice)
-label.image=alldice
-"""
+def add_heading(root):
+    tk.Label(root, text="").pack()
+    heading1 = tk.Label(root, text='Dice Simulator', fg='white', bg='black', font=("", 44))
+    heading1.pack()
+    tk.Label(root, text="").pack()
 
-dice = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png']
-# simulating the dice with random numbers between
-# 0 to 6 and generating image
-DiceImage = ImageTk.PhotoImage(Image.open('C:/Users/Arzoo/Desktop/DiceSimulation/'+random.choice(dice)))
+def add_dice_and_button(root, side, btn_text, biased=False):
+    # Load initial image
+    fp = prepare_open_path(pick_image(biased))
+    img = load_photo(fp)
+    lbl = tk.Label(root, image=img)
+    lbl.image = img
+    lbl.pack(side=side, expand=True)
 
-# construct a label widget for image
-ImageLabel = tkinter.Label(root, image=DiceImage)
-ImageLabel.image = DiceImage
-ImageLabel.place(anchor='sw')
+    def on_roll():
+        fp2 = prepare_open_path(pick_image(biased))
+        new_img = load_photo(fp2)
+        lbl.configure(image=new_img)
+        lbl.image = new_img
 
-# packing a widget in the parent widget 
-ImageLabel.pack( side=tkinter.LEFT,expand=True)
+    btn = tk.Button(root, text=btn_text, fg='black', bg='white',
+                    height=2, width=30, command=on_roll)
+    btn.pack(side=side, expand=True)
 
-def roll_dice():
-    DiceImage = ImageTk.PhotoImage(Image.open('C:/Users/Arzoo/Desktop/DiceSimulation/'+random.choice(dice)))
-    ImageLabel.configure(image=DiceImage)
-    ImageLabel.image=DiceImage
-button=tkinter.Button(root,text="Roll the normal Dice",fg='black',bg='white',height=2,width=15,command=roll_dice)
-button.pack(side=tkinter.LEFT,expand=True)
+# --- Main ---
+def main():
+    root = setup_root()
+    add_heading(root)
 
-dice1 = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png','6.png']
-# simulating the dice with random numbers between
-# 0 to 6 and generating image
-DiceImage1 = ImageTk.PhotoImage(Image.open('C:/Users/Arzoo/Desktop/DiceSimulation/'+random.choice(dice1)))
+    add_dice_and_button(root, side=tk.LEFT, btn_text="Roll the normal Dice", biased=False)
+    add_dice_and_button(root, side=tk.RIGHT, btn_text="Roll the Dice with high prob of 6", biased=True)
 
-# construct a label widget for image
-ImageLabel1 = tkinter.Label(root, image=DiceImage1)
-ImageLabel1.image = DiceImage1
+    root.mainloop()
 
-# packing a widget in the parent widget 
-ImageLabel1.pack(side=tkinter.RIGHT, expand=True)
-
-def roll_dice1():
-    DiceImage1 = ImageTk.PhotoImage(Image.open('C:/Users/Arzoo/Desktop/DiceSimulation/'+random.choice(dice1)))
-    ImageLabel1.configure(image=DiceImage1)
-    ImageLabel1.image=DiceImage1
-button1=tkinter.Button(root,text="Roll the Dice with high prob of 6",fg='black',bg='white',height=2,command=roll_dice1)
-button1.pack(side=tkinter.RIGHT,expand=True)
-root.mainloop()
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Dice-Simulation
 There are two types of Dice. First one is normal dice. Number is selected randomly and probability of each number to get selected is same. Second dice is customized dice. Probability of number 6 is higher than other numbers to get selected.
+
+UI Design:
+![alt text](image-1.png)
